77dbbd1dfb499b766fbd777ce0a71ef330d68200
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-10-15 19:07:02 +0200

    Implement `filterByAge` and `groupByName`

--- lab1/src/main/java/ua/mibal/Launcher.java
+++ lab1/src/main/java/ua/mibal/Launcher.java
@@ -4,8 +4,13 @@ import ua.mibal.component.Gatherer;
 import ua.mibal.component.Generator;
 import ua.mibal.model.Participant;
 
+import java.time.Period;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
+
+import static java.time.LocalDate.now;
+import static java.util.stream.Collectors.groupingBy;
 
 /**
  * @author Mykhailo Balakhon
@@ -15,7 +20,32 @@ public class Launcher {
 
     public static void main(String[] args) {
         Stream<Participant> participants = Generator.generate();
-        List<Participant> result = Gatherer.gather(participants, "Київ", 10, 500);
-        result.forEach(System.out::println);
+        List<Participant> gathered = Gatherer.gather(participants, "Київ", 10, 500);
+
+        List<Participant> filteredByAge = filterByAge(gathered, 20, 35);
+        Map<String, List<Participant>> groupedByName = groupByName(filteredByAge);
+
+        // todo 6, 7
+
+        groupedByName.forEach((name, list) -> {
+            System.out.println(name + ":");
+            list.forEach(System.out::println);
+            System.out.println();
+        });
+    }
+
+    private static Map<String, List<Participant>> groupByName(List<Participant> participants) {
+        return participants.stream()
+                .collect(groupingBy(Participant::firstName));
+    }
+
+    private static List<Participant> filterByAge(List<Participant> participants, int min, int max) {
+        return participants.stream()
+                .filter(p -> {
+                    Period period = Period.between(p.birthDate(), now());
+                    int age = period.getYears();
+                    return min <= age && age <= max;
+                })
+                .toList();
     }
 }
