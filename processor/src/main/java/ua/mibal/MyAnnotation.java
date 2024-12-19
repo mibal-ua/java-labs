6ed6493498b2cccb8e073c262df0079c98b181cf
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-12-19 20:55:07 +0100

    Processor: Pilot version

---
+++ processor/src/main/java/ua/mibal/MyAnnotation.java
@@ -0,0 +1,15 @@
+package ua.mibal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface MyAnnotation {
+}
