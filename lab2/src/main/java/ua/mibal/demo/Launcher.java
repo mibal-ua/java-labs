4e275b5a6554059415e2d0e5a44e015117f1bfdc
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-12-19 22:40:15 +0100

    Client: Use new API

--- lab2/src/main/java/ua/mibal/demo/Launcher.java
+++ lab2/src/main/java/ua/mibal/demo/Launcher.java
@@ -1,11 +1,12 @@
 package ua.mibal.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import ua.mibal.codegen.api.Serializers;
 import ua.mibal.demo.model.Book;
 import ua.mibal.demo.model.Car;
 import ua.mibal.demo.model.NonSerializable;
 import ua.mibal.demo.model.Transaction;
-import ua.mibal.serializer.reflection.Serializer;
+import ua.mibal.serializer.codegen.Serializer;
 import ua.mibal.serializer.reflection.exception.SerializationException;
 
 /**
@@ -14,7 +15,7 @@ import ua.mibal.serializer.reflection.exception.SerializationException;
  */
 @Slf4j
 public class Launcher {
-    private static final Serializer serializer = new Serializer();
+    private static final Serializer serializer = Serializers.getInstance(Serializer.class);
 
     public static void main(String[] args) {
         Book cleanCode = Book.builder()
@@ -50,7 +51,7 @@ public class Launcher {
             log.error("Keep calm, expected error during serialization", e);
         }
     }
-    
+
     private static void serialize(Object object) {
         String className = object.getClass().getSimpleName();
         log.info("{} serialization demo", className);
@@ -58,10 +59,10 @@ public class Launcher {
 
         String xml = serializer.xml(object);
         log.info("{}: {}", className, xml);
-        
+
         String json = serializer.json(object);
         log.info("{}: {}", className, json);
-        
+
         log.info("");
     }
 }
---
+++ lab2/src/main/java/ua/mibal/serializer/codegen/Serializer.java
@@ -0,0 +1,12 @@
+package ua.mibal.serializer.codegen;
+
+import ua.mibal.codegen.api.GenerateSerializer;
+import ua.mibal.codegen.api.GeneratedSerializer;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
+ */
+@GenerateSerializer
+public interface Serializer extends GeneratedSerializer {
+}
