495953113749ce119f1d62de1f36fdb28d4f76fe
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-12-19 13:23:30 +0100

    Separate general login to `ModelValidator`

---
+++ lab2/src/main/java/ua/mibal/serializer/component/ModelValidator.java
@@ -0,0 +1,30 @@
+package ua.mibal.serializer.component;
+
+import ua.mibal.serializer.exception.XmlSerializationException;
+
+import java.io.Serializable;
+
+import static java.util.Arrays.stream;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
+ */
+public class ModelValidator {
+
+    public void validate(Object model) {
+        validateSerializableInterface(model);
+    }
+
+    private void validateSerializableInterface(Object model) {
+        Class<?>[] interfaces = model.getClass().getInterfaces();
+        if (stream(interfaces).noneMatch(
+                c -> c.equals(Serializable.class))
+        ) {
+            throw new XmlSerializationException(
+                    "Model can not be serialized. " +
+                    "Model class should implement Serializable interface"
+            );
+        }
+    }
+}
--- lab2/src/main/java/ua/mibal/serializer/component/XmlModelValidator.java
+++ lab2/src/main/java/ua/mibal/serializer/component/XmlModelValidator.java
@@ -3,19 +3,15 @@ package ua.mibal.serializer.component;
 import ua.mibal.serializer.annotation.XmlModel;
 import ua.mibal.serializer.exception.XmlSerializationException;
 
-import java.io.Serializable;
-
-import static java.util.Arrays.stream;
-
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
-public class XmlModelValidator {
+public class XmlModelValidator extends ModelValidator {
 
     public void validate(Object model) {
         validateXmlModelAnnotation(model);
-        validateSerializableInterface(model);
+        super.validate(model);
     }
 
     private void validateXmlModelAnnotation(Object model) {
@@ -24,18 +20,6 @@ public class XmlModelValidator {
             throw new XmlSerializationException(
                     "Model can not be serialized. " +
                     "Model class should be annotated with " + XmlModel.class.getSimpleName() + " annotation"
-            );
-        }
-    }
-
-    private void validateSerializableInterface(Object model) {
-        Class<?>[] interfaces = model.getClass().getInterfaces();
-        if (stream(interfaces).noneMatch(
-                c -> c.equals(Serializable.class))
-        ) {
-            throw new XmlSerializationException(
-                    "Model can not be serialized. " +
-                    "Model class should implement Serializable interface"
             );
         }
     }
